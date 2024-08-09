b0b6c9f449e5e94e0b73c5b31acf9ac8ddd369b9
vanphutin2004 <vanphutin.contact@gmail.com>
2024-08-09 12:35:01 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import InformationTechnologyStudent from "./_chaprer_four/extends/InformationTechnologyStudent";
+
+let SinhVienA = new InformationTechnologyStudent(
+  1,
+  "Nguyen Thanh",
+  "Tung",
+  21,
+  true,
+  10,
+  10,
+  10
+);
+console.log(SinhVienA.toString());
