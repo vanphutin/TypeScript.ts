d84980863cd7f312db0185bdebbfe7a302a6bbe9
vanphutin2004 <vanphutin.contact@gmail.com>
2024-11-05 00:38:53 +0700

    quan li nhan vien

--- src/OOP/Quan-Li-Nhan-Vien/SoftwareHouse.ts
+++ src/OOP/Quan-Li-Nhan-Vien/SoftwareHouse.ts
@@ -72,7 +72,7 @@ class SoftwareHouse {
   // Print listStaff details
   public printF(): void {
     for (const item of this.listStaff) {
-      console.log(item);
+      console.table(item);
     }
   }
 
@@ -80,7 +80,9 @@ class SoftwareHouse {
   public addEmployee(employee: Employee): boolean {
     // Kiểm tra xem có nhân viên nào có cùng mã nhân viên không
     const exists = this.listStaff.some(
-      (item) => item.getThePayrollNunber() === employee.getThePayrollNunber()
+      (item) =>
+        item instanceof Employee &&
+        item.getThePayrollNunber() === employee.getThePayrollNunber()
     );
     // console.log(exists);
     if (exists) {
@@ -92,13 +94,80 @@ class SoftwareHouse {
     this.listStaff.push(employee);
     return true;
   }
+
+  //tính tổng tiền phải trả cho các nhân viên
+
+  public getMonthlySalaryBill(): number {
+    let totalSalaryBill = this.listStaff.reduce(
+      (accumulator, item) => accumulator + item.getMonthlySalary(),
+      0
+    );
+    return totalSalaryBill;
+  }
+
+  //sắp xếp danh sách nhân viên theo tên
+  sortAllStaffByName(): void {
+    const sortStaffByName = this.listStaff.sort((a, b) => {
+      return a.getTheName().localeCompare(b.getTheName());
+    });
+    console.log(sortStaffByName);
+  }
+
+  //hiển thị danh sách các lập trình viên.
+  displayListProgrammers(): void {
+    const programmers = this.listStaff.filter(
+      (item) => item instanceof Programmer
+    );
+    console.log(programmers);
+  }
+
+  //, cập nhật phòng ban là deptNew cho người quản lý có mã số là aPayrollNo, trả về true nếu cập nhật thành công.
+  public updateDepartmentForAdmin(
+    aPayrollNo: number,
+    deptNew: string
+  ): boolean {
+    const admin = this.listStaff.find(
+      (item) => item.getThePayrollNunber() === aPayrollNo
+    );
+    if (admin) {
+      admin.setTheName(deptNew);
+      return true;
+    }
+    // for (const x of this.listStaff) {
+    //   if (!x.getThePayrollNunber().toString().includes(aPayrollNo.toString())) {
+    //     return false;
+    //   }
+    //   x.setTheName(deptNew);
+    //   return true;
+    // }
+    return false;
+  }
+
+  //xóa nhân viên theo mã số, trả về true nếu xóa được.
+  public deleteEmployee(id: number): boolean {
+    const initialLength = this.listStaff.length;
+
+    this.listStaff = this.listStaff.filter(
+      (item) => item.getThePayrollNunber() !== id
+    );
+
+    return this.listStaff.length < initialLength;
+  }
 }
 
 // Example usage
 const softwareHouse = new SoftwareHouse("GlobalTech");
 softwareHouse.readFile("SoftwareHouse.json");
 
-const programmer = new Programmer(2213, "Van Phu Tin", 3000, "JavaScript");
-softwareHouse.addEmployee(programmer);
-console.log(softwareHouse.addEmployee(programmer) ? "successful" : "fail");
+// const programmer = new Programmer(223, "Van Phu Tin", 3000, "JavaScript");
 // softwareHouse.printF();
+// softwareHouse.addEmployee(programmer);
+// console.log(softwareHouse.addEmployee(programmer) ? "successful" : "fail");
+// console.log(softwareHouse.getMonthlySalaryBill());
+// softwareHouse.sortAllStaffByName();
+// softwareHouse.displayListProgrammers();
+// console.log(softwareHouse.updateDepartmentForAdmin(222, "hehe"));
+// console.log(softwareHouse.deleteEmployee(222));
+softwareHouse.printF();
+
+// HOÀN THÀNH 98%
